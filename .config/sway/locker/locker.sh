e41fc1c986c0a0154a0d1a735f7b509645742e9c
rustysec <russ@infocyte.com>
2023-03-01 10:07:24 -0800

    updating

--- .config/sway/locker/locker.sh
+++ .config/sway/locker/locker.sh
@@ -1,12 +1,12 @@
 swaylock -f -c 282c34 \
     --inside-color 282c34 \
     --inside-wrong-color ed1515 \
-    --ring-wrong-color c0392b \
+    --ring-wrong-color ed1515 \
     --inside-ver-color f67400 \
-    --ring-ver-color fdbc4b \
+    --ring-ver-color f67400 \
     --ring-color 1d99f3 \
     --ring-clear-color 1b668f \
-    --line-clear-color 1abc9c \
+    --line-clear-color 1b668f \
     --inside-clear-color 16a085 \
     --key-hl-color 3daee9 \
     --bs-hl-color b65619
