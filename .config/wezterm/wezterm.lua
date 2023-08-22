ffe0e2b8461320c7acf423616c3956fb4e063940
rustysec <russ@infocyte.com>
2023-08-21 18:07:28 -0700

    minor tweaks

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,8 @@
 return {
-    color_scheme = 'Breeze',
-    font_size = 10.0,
-    enable_tab_bar = false,
-    window_background_opacity = 0.95,
-    scrollback_lines = 0,
-    window_decorations = 'NONE',
+	color_scheme = "TokyoNight",
+	font_size = 10.0,
+	enable_tab_bar = false,
+	window_background_opacity = 0.95,
+	scrollback_lines = 0,
+	window_decorations = "NONE",
 }
