c346c0c036f765075dcf3a6c4381a7316ce9b8d6
rustysec <russ@infocyte.com>
2023-12-03 08:19:08 -0800

    formatting + polkit

--- .config/sway/helpers/polkit.sh
+++ .config/sway/helpers/polkit.sh
@@ -2,19 +2,18 @@
 
 # start a polkit agent
 AGENTS=(
-    "/usr/libexec/polkit-mate-authentication-agent-1"
-    "/usr/libexec/polkit-gnome-authentication-agent-1"
-    "/usr/libexec/polkit-kde-authentication-agent-1"
-    "/usr/lib/x86_64-linux-gnu/libexec/polkit-kde-authentication-agent-1"
-    "/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1"
-    "/usr/lib/x86_64-linux-gnu/polkit-mate/polkit-mate-authentication-agent-1"
+	"/usr/libexec/polkit-gnome-authentication-agent-1"
+	"/usr/libexec/polkit-mate-authentication-agent-1"
+	"/usr/libexec/polkit-kde-authentication-agent-1"
+	"/usr/lib/x86_64-linux-gnu/libexec/polkit-kde-authentication-agent-1"
+	"/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1"
+	"/usr/lib/x86_64-linux-gnu/polkit-mate/polkit-mate-authentication-agent-1"
 )
 
-for agent in ${AGENTS[@]};
-do
-    if [ -f $agent ]; then
-        echo "Using $agent"
-        $agent
-        break
-    fi
+for agent in ${AGENTS[@]}; do
+	if [ -f $agent ]; then
+		echo "Using $agent"
+		$agent
+		break
+	fi
 done
